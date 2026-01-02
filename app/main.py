eb1adf508f4ee59528d573beb258e6ed4e36540e
aachaltiwari <mail2aachal2030@gmail.com>
2026-01-02 13:04:55 +0545

    converting starllette server into FastApi

--- app/main.py
+++ app/main.py
@@ -1,15 +1,45 @@
-from starlette.applications import Starlette
-from starlette.routing import Mount
+# from starlette.applications import Starlette
+# from starlette.routing import Mount
+# from contextlib import asynccontextmanager
+
+# from app.core.database import connect_mongo, close_mongo
+
+# from app.graphql.schema import graphql_app
+# from app.middlewares.auth import AuthMiddleware
+
+
+# @asynccontextmanager
+# async def lifespan(app: Starlette):
+#     # Startup
+#     await connect_mongo(app)
+#     yield
+#     # Shutdown
+#     await close_mongo(app)
+
+
+# app = Starlette(
+#     debug=True,
+#     lifespan=lifespan,
+#     routes=[
+#         Mount("/graphql", graphql_app),
+        
+#     ],
+# )
+
+
+# app.add_middleware(AuthMiddleware)
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from app.core.database import connect_mongo, close_mongo
-
 from app.graphql.schema import graphql_app
 from app.middlewares.auth import AuthMiddleware
 
-
 @asynccontextmanager
-async def lifespan(app: Starlette):
+async def lifespan(app: FastAPI):
     # Startup
     await connect_mongo(app)
     yield
@@ -17,19 +47,18 @@ async def lifespan(app: Starlette):
     await close_mongo(app)
 
 
-app = Starlette(
+app = FastAPI(
     debug=True,
     lifespan=lifespan,
-    routes=[
-        Mount("/graphql", graphql_app),
-        
-    ],
 )
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(AuthMiddleware)
 
-
-
-
-
+app.mount("/graphql", graphql_app)
