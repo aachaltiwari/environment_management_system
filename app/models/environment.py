f5d8ed71945917a23f3072f94b456a9eb9b98c4a
aachaltiwari <mail2aachal2030@gmail.com>
2026-01-13 13:27:22 +0545

    environemnt models created

---
+++ app/models/environment.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pydantic import Field
+from app.models.base import MongoBaseModel, PyObjectId
+from typing import Any, Dict
+
+
+class EnvironmentTypeModel(MongoBaseModel):
+    name: str = Field(..., min_length=2)
+    description: str | None = None
+    created_at: datetime
+
+
+
+class EnvironmentModel(MongoBaseModel):
+    integration_id: PyObjectId
+    environment_type: PyObjectId
+
+    title: str = Field(..., min_length=2)
+    content: Dict[str, Any]
+
+    created_by: PyObjectId
+    updated_by: PyObjectId
+
+    created_at: datetime
+    updated_at: datetime
