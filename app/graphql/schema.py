f6f05a0660afd125c55b29b30c7759244cb733a4
aachaltiwari <mail2aachal2030@gmail.com>
2026-01-15 10:33:13 +0545

    custom json scalar defined

--- app/graphql/schema.py
+++ app/graphql/schema.py
@@ -1,15 +1,26 @@
 from ariadne.asgi import GraphQL
 from app.graphql.context import get_context_value
-from ariadne import load_schema_from_path, make_executable_schema
+from ariadne import ScalarType, load_schema_from_path, make_executable_schema
 from app.graphql.resolvers.query import query
 from app.graphql.resolvers.mutation import mutation
+import json
 
 type_defs = load_schema_from_path("app/graphql/schema.graphql") 
+
+json_scalar = ScalarType("JSON")
+
+@json_scalar.serializer
+def serialize_json(value):
+    return value
+
+@json_scalar.value_parser
+def parse_json_value(value):
+    return value
 
 schema = make_executable_schema(
     type_defs,
     query,
-    mutation,
+    mutation, json_scalar
 )
 
 
